bc4fd8a85f461a6d87cdecb76a18bd7deeac15d5
Nahid <md.nahid186502@gmail.com>
2026-09-23 17:22:20 +0600

    Fixed all problems and finally project completed

--- src/lib/FetchApp.ts
+++ src/lib/FetchApp.ts
@@ -1,11 +1,10 @@
-
-export const getApps = async() => {
+export const getApps = async () => {
     try {
-        const response = await fetch("http://localhost:3000/data.json");
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data.json`);
         const data = await response.json();
         return data;
-    }catch(error){
+    } catch (error) {
         console.log("Error fetching apps data", error);
-        return[];
-    };
-}
+        return [];
+    }
+};
